bfe98247b414c4bd217210cd6aad3aa093ce61b2
Kyle Kaminky <kyle.kaminky@gmail.com>
2023-01-18 20:42:24 -0700

    Pushing website

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+jQuery(window).load(function() {
+  $('.preloader').fadeOut('slow');
+});
+*/
+
+$(window).on('beforeunload', function() {
+  $(window).scrollTop(0);
+});
+
+window.addEventListener('load', () => {
+  const preload = document.querySelector('.preloader');
+  const loading = document.querySelector('.loading');
+  setTimeout( function() {
+      preload.classList.add('preloader-finish');
+      loading.classList.remove('loading');
+  }, 2000 );
+});
+
